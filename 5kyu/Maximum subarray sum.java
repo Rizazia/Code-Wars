e87f93009e2eb43742a625718921233352b29eec
Rizazia <71036120+Rizazia@users.noreply.github.com>
2021-09-23 16:15:16 -0700

    Create Maximum subarray sum.java

---
+++ 5kyu/Maximum subarray sum.java
@@ -0,0 +1,47 @@
+/*
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+// should be 6: {4, -1, 2, 1}
+
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+*/
+public class Max {
+  public static int sequence(int[] arr) {
+    //empty array
+    if (arr.length == 0) return 0;
+    
+    //all elements are positive
+    int maximumSum = 0;
+    if (allPositiveElements(arr)) {
+      for (int i = 0; i < arr.length; i++){
+        maximumSum += arr[i];
+      }
+      return maximumSum;
+    }
+    
+    //find the highest sub-array
+    for (int i = 0; i < arr.length; i++) {
+			int currentSum = 0;
+			currentSum += arr[i];
+			for (int j = i; j < arr.length; j++) {
+				if (j != arr.length - 1) {
+					currentSum += arr[j + 1];
+				}
+				if (currentSum > maximumSum) {
+					maximumSum = currentSum;
+				}
+			}
+		}
+    
+    return maximumSum;
+  }
+  
+  public static boolean allPositiveElements(int[] arr){
+    for (int i = 0; i < arr.length; i++){
+      if (arr[i] < 0) return false;
+    }
+    return true;
+  }
