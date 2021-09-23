f2e3cf241474a99f4fcb90b34a6b871b25ebddc4
Rizazia <71036120+Rizazia@users.noreply.github.com>
2021-09-22 17:04:42 -0700

    Create Next bigger number with the same digits.java

---
+++ 4kyu/Next bigger number with the same digits.java
@@ -0,0 +1,70 @@
+/*
+Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+
+12 ==> 21
+513 ==> 531
+2017 ==> 2071
+
+nextBigger(num: 12)   // returns 21
+nextBigger(num: 513)  // returns 531
+nextBigger(num: 2017) // returns 2071
+
+If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift):
+
+9 ==> -1
+111 ==> -1
+531 ==> -1
+
+nextBigger(num: 9)   // returns nil
+nextBigger(num: 111) // returns nil
+nextBigger(num: 531) // returns nil
+
+*/
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Kata{
+    public static long nextBiggerNumber(long n){
+      if (n < 10) return -1;
+
+      StringBuilder num = new StringBuilder();
+
+      String str = String.valueOf(n);
+      for (int i = str.length() - 1; i > 0; i--) {
+        // find the first place from the right where the left-digit is less than the right one
+        final int digit = Character.getNumericValue(str.charAt(i - 1));
+        if (digit < Character.getNumericValue(str.charAt(i))) {
+
+            // find the smallest digit that is larger than left-digit
+            int smallestLargerDigit = 9;
+            int indexOfSmallestLargerDigit = 0;
+            for (int j = i; j < str.length(); j++) {
+                final int actualDigit = Character.getNumericValue(str.charAt(j));
+                if (actualDigit > digit && actualDigit <= smallestLargerDigit) {
+                    smallestLargerDigit = actualDigit;
+                    indexOfSmallestLargerDigit = j;
+                }
+            }
+
+            // collect the righter digits except the smallest larger one
+            final ArrayList<Integer> digits = new ArrayList<>();
+            for (int j = i - 1; j < str.length(); j++) {
+                if (j != indexOfSmallestLargerDigit) {
+                    digits.add(Character.getNumericValue(str.charAt(j)));
+                }
+            }
+
+            num.append(str.substring(0, i - 1));
+            num.append(smallestLargerDigit);
+
+            // sort the digits
+            Collections.sort(digits);
+            for (int actualDigit : digits) {
+                num.append(actualDigit);
+            }
+            return Long.parseLong(num.toString());
+        }
+    }
+    return -1;
+  }
+}
